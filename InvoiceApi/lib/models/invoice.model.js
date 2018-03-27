7480efb5cac258c8dbeb4c49c2e259650ec019d9
Lars Heesakkers <larsheesakkers@live.nl>
2018-03-27 16:14:45 +0200

    invoice api

---
+++ InvoiceApi/lib/models/invoice.model.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const InvoiceSchema = new Schema({
+  uuid: { type: String, required: true },
+  paid: { type: String, required: true  },
+  registerId: [{type: String}],
+  currentWage: { type: Number, required: true },
+});
+
+module.exports = {
+  invoice: mongoose.model('Invoice', InvoiceSchema)
+};
