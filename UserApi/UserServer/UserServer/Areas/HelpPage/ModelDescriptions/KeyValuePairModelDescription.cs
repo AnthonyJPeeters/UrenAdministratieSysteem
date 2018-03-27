403b15879f7624a763e8cb6d837149aec74c9d24
Anthony Peeters <anthonypeeters221@hotmail.com>
2018-03-27 13:23:26 +0200

    added project

---
+++ UserApi/UserServer/UserServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace UserServer.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
